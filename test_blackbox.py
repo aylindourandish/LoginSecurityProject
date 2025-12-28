ce01a446171c2bec888de9742233012a435bc6c7
Aylin <aylindourandishh@gmail.com>
2025-12-28 11:58:32 +0330

    Refactor print statements in black box test

--- test_blackbox.py
+++ test_blackbox.py
@@ -2,30 +2,30 @@ import os;os.system('cls')
 from main import login, check_password_strength
 
 def black_box_test():
-    # print("=" * 50)
-    print("🧪 Black Box Test: Validating Program's Judgment")
-    # print("=" * 50)
+
+    print(" Black Box Test: Validating Program's Judgment")
+   
 
     test_count = 1
 
     while True:
-        # print(f"\n --- 🧪 Test Case #{test_count} ---")
-        username = input("\n👤 Enter username: ").strip()
-        password = input("🔑 Enter password: ").strip()
+        
+        username = input("\n Enter username: ").strip()
+        password = input(" Enter password: ").strip()
 
-        print(f"\n --- 🧪 Test Case #{test_count} ---")
+        print(f"\n ---  Test Case #{test_count} ---")
         expected_strength = check_password_strength(password)
         output = login(username, password)
 
-        print("\n📤 Program Output:")
+        print("\n Program Output:")
         print(f"       {output}")
 
         if f"Password strength: {expected_strength}" in output:
-            print("✅ Test Result: PASSED (Program judged password correctly)\n")
+            print(" Test Result: PASSED (Program judged password correctly)\n")
         else:
-            print("❌ Test Result: FAILED (Program misjudged password strength)\n")
+            print(" Test Result: FAILED (Program misjudged password strength)\n")
 
-        print("🔁🛑 Test Finished. Starting next test...\n\n")
+        print(" Test Finished. Starting next test...\n\n")
         print("-------------------\n")
         test_count += 1
 
